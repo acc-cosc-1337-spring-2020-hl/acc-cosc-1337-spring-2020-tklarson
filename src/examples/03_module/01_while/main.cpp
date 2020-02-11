74c494848771e80c687d5d5918a4efca29081f95
tklarson <trevor.larson@g.austincc.edu>
2020-02-10 18:55:41 -0600

    While, For, Do While functions

--- src/examples/03_module/01_while/main.cpp
+++ src/examples/03_module/01_while/main.cpp
@@ -1,8 +1,12 @@
-#include "sample_while.h"
+#include<iostream>
+#include "while.h"
+
+using std::cout;
 
 int main() 
 {
-	
+	int sum = sum_of_squares(5);
+	cout << "Result is: " << sum << "\n";
 
 	return 0;
 }
