cd6ea4832d27ca9ed3effb7ca5892cf7bae2e97d
tklarson <trevor.larson@g.austincc.edu>
2020-02-24 19:01:16 -0600

    bank account deposit and withdraw and get balance

--- test/examples_test/04_module_test/04_module_tests.cpp
+++ test/examples_test/04_module_test/04_module_tests.cpp
@@ -1,7 +1,44 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "bank_account.h"
 
 TEST_CASE("Verify Test Configuration", "verification") {
 	REQUIRE(true == true);
 }
 
+TEST_CASE("Test Bank Account get_balance")
+{
+	BankAccount account(150);
+
+	REQUIRE(account.get_balance() == 150);
+
+}
+
+TEST_CASE("Test Bank Account deposit")
+{
+	BankAccount account(150);
+	REQUIRE(account.get_balance() == 150);
+	
+	account.deposit(-50);
+	REQUIRE(account.get_balance() == 150);
+
+	account.deposit(50);
+	REQUIRE(account.get_balance() == 200);
+	
+}
+
+TEST_CASE("Test Bank Account withdraw")
+{
+	BankAccount account(150);
+	REQUIRE(account.get_balance() == 150);
+
+	account.withdraw(-5);
+	REQUIRE(account.get_balance() == 150);
+	
+	account.withdraw(50);
+	REQUIRE(account.get_balance() == 100);
+
+	account.withdraw(101);
+	REQUIRE(account.get_balance() == 100);
+
+}
