887a0f2f380ce982dca6472df3def821c461fc8d
tklarson <trevor.larson@g.austincc.edu>
2020-03-04 17:30:30 -0600

    HW 5

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -1,25 +1,33 @@
 //tic_taco_toe.cpp
 
 #include "tic_tac_toe.h"
+using std::string;
 
-void start_game(string first_player)
+void TicTacToe :: start_game(string first_player)
+{
+	if(first_player == "X" || first_player == "O")
+	{
+		player = first_player;
+		
+	}
+	else
+	{
+		throw Error("\nPlayer must be X or O");
+	}
+}
+
+void TicTacToe::mark_board(int position)
 {
 
 }
 
-void mark_board(int position)
+string TicTacToe::get_player() const
 {
 
 }
 
-string get_player() const
+void TicTacToe::set_next_player()
 {
 
 }
 
-void set_next_player()
-{
-
-}
-
-string player
--- src/homework/tic_tac_toe/tic_tac_toe.h
+++ src/homework/tic_tac_toe/tic_tac_toe.h
@@ -1,23 +1,27 @@
 //tic_taco_toe.h
 #include<string>
+using std::string;
 
 class TicTacToe
 {
 public:
+	void start_game(std::string first_player);
+	void mark_board(int position);
+	string get_player() const;
+	void set_next_player();
 
 private:
-
+	string player;
 };
 
 
 class Error
 {
 public:
-	Error(std::string msg)
-
-	std::string get_message
+	Error(std::string msg) : message{ msg } {}
+	std::string get_message()const { return message; }
 
 private:
-	std::string message
+	std::string message;
 
 };
