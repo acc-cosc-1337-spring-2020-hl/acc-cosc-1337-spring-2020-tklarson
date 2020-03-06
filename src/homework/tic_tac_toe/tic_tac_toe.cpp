8abb86969ff2efd45dc71685a02cd7a7744e83ea
Trevor <trevor.larson@g.austincc.edu>
2020-03-06 09:04:57 -0600

    HW changes

--- src/homework/tic_tac_toe/tic_tac_toe.cpp
+++ src/homework/tic_tac_toe/tic_tac_toe.cpp
@@ -18,7 +18,14 @@ void TicTacToe :: start_game(string first_player)
 
 void TicTacToe::mark_board(int position)
 {
-
+	if (position == 1, 2, 3, 4, 5, 6, 7, 8, 9 )
+	{
+		position = position;
+	}
+	else
+	{
+		throw Error("Position must be 1 to 9");
+	}
 }
 
 string TicTacToe::get_player() const
@@ -28,6 +35,13 @@ string TicTacToe::get_player() const
 
 void TicTacToe::set_next_player()
 {
-
+	if (player == "X")
+	{
+		set_next_player("O");
+	}
+	else
+	{
+		player = "X";
+	}
 }
 
--- test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
+++ test/homework_test/tic_tac_toe_test/tic_tac_toe_test.cpp
@@ -1,5 +1,6 @@
 #define CATCH_CONFIG_MAIN  // This tells Catch to provide a main() - only do this in one cpp file
 #include "catch.hpp"
+#include "tic_tac_toe.h"
 
 TEST_CASE("Verify Test Configuration", "verification") 
 {
@@ -8,22 +9,38 @@ TEST_CASE("Verify Test Configuration", "verification")
 
 TEST_CASE("Test can’t call mark board before start game")
 {
+	TicTacToe mark_board(1);
+	REQUIRE(mark_baord() == 1);
 
+	REQUIRE_THROWS_AS(mark_baord(10),
+		Error);
 }
 
 TEST_CASE("Test start game accepts only X or O")
 {
+	TicTacToe start_game("X");
+	REQUIRE(first_player() == "X");
 
+	REQUIRE_THROWS_AS(first_player("V"),
+		Error);
 }
 
 TEST_CASE("Test set first player to X")
 {
+	TicTacToe start_game(X);
+	REQUIRE(first_player() == X);
 
+	REQUIRE_THROWS_AS(first_player(V),
+		Error);
 }
 
 TEST_CASE("Test set first player to O")
 {
+	TicTacToe start_game("O");
+	REQUIRE(first_player() == "O");
 
+	REQUIRE_THROWS_AS(first_player("V"),
+		Error);
 }
 
 TEST_CASE("Test start game with X game flow")
